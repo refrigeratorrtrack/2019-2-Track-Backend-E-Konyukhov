ca3859985bd7b088606e5a92cf0d9964e6b77b4b
refrigeratorrtrack <e.refrigeratorr@gmail.com>
2019-10-10 19:07:28 +0300

    Fixed indentation, fixed for PEP8 requirements

--- tests.py
+++ tests.py
@@ -1,38 +1,38 @@
 import unittest
 import tictactoe
 
+
 class TestGameMethods(unittest.TestCase):
-	""" Tests. """
-	def test_check_win(self):
-		self.game = tictactoe.TicTacToe()
-		self.game.playingField = ["X", "X", "X", 4, 5, 6, 7, 8, 9]
-		self.game.check_win()
-		self.assertTrue(self.game.win[0])
-		self.game = tictactoe.TicTacToe()
-		self.game.playingField = ["X", "0", "X", 4, 5, 6, 7, 8, 9]
-		self.game.check_win()
-		self.assertFalse(self.game.win[0])
+    """ Tests. """
+    def test_check_win(self):
+        self.game = tictactoe.TicTacToe()
+        self.game.playingField = ["X", "X", "X", 4, 5, 6, 7, 8, 9]
+        self.game.check_win()
+        self.assertTrue(self.game.win[0])
+        self.game = tictactoe.TicTacToe()
+        self.game.playingField = ["X", "0", "X", 4, 5, 6, 7, 8, 9]
+        self.game.check_win()
+        self.assertFalse(self.game.win[0])
 
-	def test_print_invitation_to_move(self):
-		self.game = tictactoe.TicTacToe()
-		self.game.order = 3
-		self.assertFalse(self.game.print_invitation_to_move())
+    def test_print_invitation_to_move(self):
+        self.game = tictactoe.TicTacToe()
+        self.game.order = 3
+        self.assertFalse(self.game.print_invitation_to_move())
 
-	def test_set_move(self):
-		self.game = tictactoe.TicTacToe()
-		self.game.order = 3
-		self.assertFalse(self.game.set_move())
+    def test_set_move(self):
+        self.game = tictactoe.TicTacToe()
+        self.game.order = 3
+        self.assertFalse(self.game.set_move())
 
-	def test_check_draw(self):
-		self.game = tictactoe.TicTacToe()
-		self.game.playingField = ["X", "0", "X", "0", "X", "X", "0", "X", "0"]
-		self.game.check_draw()
-		self.assertTrue(self.game.draw)
-		self.game = tictactoe.TicTacToe()
-		self.game.playingField = ["X", "X", "X", "0", "X", "X", "0", 8, "0"]
-		self.game.check_draw()
-		self.assertFalse(self.game.draw)
-
+    def test_check_draw(self):
+        self.game = tictactoe.TicTacToe()
+        self.game.playingField = ["X", "0", "X", "0", "X", "X", "0", "X", "0"]
+        self.game.check_draw()
+        self.assertTrue(self.game.draw)
+        self.game = tictactoe.TicTacToe()
+        self.game.playingField = ["X", "X", "X", "0", "X", "X", "0", 8, "0"]
+        self.game.check_draw()
+        self.assertFalse(self.game.draw)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- tictactoe.py
+++ tictactoe.py
@@ -1,147 +1,152 @@
 class TicTacToe(object):
-	def __init__(self):
-		""" Initializing. """
-		self.fieldSize = 3
-		self.playingField = [i for i in range(1, self.fieldSize ** 2 + 1)]
-		self.checkingString = [str(i) for i in range(1, self.fieldSize ** 2 + 1)]
-		self.order = 1
-		self.win = [False, 0]
-		self.draw = False
-		self.variants = ["X", "0"]
+    def __init__(self):
+        """ Initializing. """
+        self.fieldSize = 3
+        self.playingField = [i for i in range(1, self.fieldSize ** 2 + 1)]
+        self.checkingString = [str(i) for i in range(1, self.fieldSize**2+1)]
+        self.order = 1
+        self.win = [False, 0]
+        self.draw = False
+        self.variants = ["X", "0"]
 
+    def display_playing_field(self):
+        """ Display playing field. """
+        print("{} | {} | {}".format(self.playingField[0],
+                                    self.playingField[1],
+                                    self.playingField[2]))
+        print("--+---+--")
+        print("{} | {} | {}".format(self.playingField[3],
+                                    self.playingField[4],
+                                    self.playingField[5]))
+        print("--+---+--")
+        print("{} | {} | {}".format(self.playingField[6],
+                                    self.playingField[7],
+                                    self.playingField[8]))
 
-	def display_playing_field(self):
-		""" Display playing field. """
-		print("{} | {} | {}".format(self.playingField[0], self.playingField[1], self.playingField[2]))
-		print("--+---+--")
-		print("{} | {} | {}".format(self.playingField[3], self.playingField[4], self.playingField[5]))
-		print("--+---+--")
-		print("{} | {} | {}".format(self.playingField[6], self.playingField[7], self.playingField[8]))
+    def input_user_move(self):
+        """ User input processing. """
+        while True:
+            self.move = input()
 
+            if self.move in self.checkingString:
+                if self.playingField[int(self.move) - 1] not in self.variants:
+                    self.move = int(self.move) - 1
+                    self.set_move()
+                    break
+                else:
+                    print("This cell is not free! Try again.")
+            else:
+                print("Incorrect cell! Try again!")
 
-	def input_user_move(self):
-		""" User input processing. """
-		while True:
-			self.move = input()
+    def set_move(self):
+        """ Set user move. """
+        if self.order == 1:
+            self.playingField[self.move] = "X"
+        elif self.order == 2:
+            self.playingField[self.move] = "0"
+        else:
+            return False
 
-			if self.move in self.checkingString:
-				if self.playingField[int(self.move) - 1] not in self.variants:
-					self.move = int(self.move) - 1
-					self.set_move()
-					break
-				else:
-					print("This cell is not free! Try again.")
-			else:
-				print("Incorrect cell! Try again!")
+    def check_win(self):
+        """ Checking win. """
+        if self.playingField[0] == self.playingField[1]:
+            if self.playingField[1] == self.playingField[2]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[0]) + 1
+        elif self.playingField[3] == self.playingField[4]:
+            if self.playingField[4] == self.playingField[5]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[3]) + 1
+        elif self.playingField[6] == self.playingField[7]:
+            if self.playingField[7] == self.playingField[8]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[6]) + 1
 
+        if self.playingField[0] == self.playingField[3]:
+            if self.playingField[3] == self.playingField[6]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[0]) + 1
+        elif self.playingField[1] == self.playingField[4]:
+            if self.playingField[4] == self.playingField[7]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[1]) + 1
+        elif self.playingField[2] == self.playingField[5]:
+            if self.playingField[5] == self.playingField[8]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[2]) + 1
 
-	def set_move(self):
-		""" Set user move. """
-		if self.order == 1:
-			self.playingField[self.move] = "X"
-		elif self.order == 2:
-			self.playingField[self.move] = "0"
-		else:
-			return False
+        if self.playingField[0] == self.playingField[4]:
+            if self.playingField[4] == self.playingField[8]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[0]) + 1
+        elif self.playingField[2] == self.playingField[4]:
+            if self.playingField[4] == self.playingField[6]:
+                self.win[0] = True
+                self.win[1] = self.variants.index(self.playingField[2]) + 1
 
+    def check_draw(self):
+        """ Check draw situation. """
+        self.counter = 0
 
-	def check_win(self):
-		""" Checking win. """
-		if self.playingField[0] == self.playingField[1] == self.playingField[2]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[0]) + 1
-		elif self.playingField[3] == self.playingField[4] == self.playingField[5]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[3]) + 1
-		elif self.playingField[6] == self.playingField[7] == self.playingField[8]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[6]) + 1
-		
-		if self.playingField[0] == self.playingField[3] == self.playingField[6]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[0]) + 1
-		elif self.playingField[1] == self.playingField[4] == self.playingField[7]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[1]) + 1
-		elif self.playingField[2] == self.playingField[5] == self.playingField[8]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[2]) + 1
+        for cell in self.playingField:
+            if cell == "X" or cell == "0":
+                self.counter += 1
 
-		if self.playingField[0] == self.playingField[4] == self.playingField[8]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[0]) + 1
-		elif self.playingField[2] == self.playingField[4] == self.playingField[6]:
-			self.win[0] = True
-			self.win[1] = self.variants.index(self.playingField[2]) + 1
+        if self.counter == 9:
+            self.draw = True
 
+    def print_congratulation(self):
+        """ Print congratulation to winner. """
+        print("Congratulations!")
 
-	def check_draw(self):
-		""" Check draw situation. """
-		self.counter = 0
+        if self.draw:
+            print("It is a draw! Both win!")
 
-		for cell in self.playingField:
-			if cell == "X" or cell == "0":
-				self.counter += 1
+        if self.win[0]:
+            if (self.win[1] == 1):
+                print("First user wins!")
+            elif (self.win[1] == 2):
+                print("Second user wins!")
+            elif self.win[1] == 3:
+                print("Draw!")
 
-		if self.counter == 9:
-			self.draw = True
+    def print_hello(self):
+        """ Print hello-message to user(s). """
+        print("Hello! You are in tic tac toe. Press 'Enter' to start game!")
 
+    def print_invitation_to_move(self):
+        """ Print invitation for users. """
+        if self.order == 1:
+            print("Move for first player.")
+        elif self.order == 2:
+            print("Move for second player.")
+        else:
+            return False
 
-	def print_congratulation(self):
-		""" Print congratulation to winner. """
-		print("Congratulations!")
+        print("Choose free cell.")
 
-		if self.draw == True:
-			print("It is a draw! Both win!")
+    def play(self):
+        """ The main part. Start game process. """
+        self.print_hello()
+        self.display_playing_field()
 
-		if self.win[0] == True:
-			if (self.win[1] == 1):
-				print("First user wins!")
-			elif (self.win[1] == 2):
-				print("Second user wins!")
-			elif self.win[1] == 3:
-				print("Draw!")
+        # while self.win[0] != True and self.draw != True:
+        while not self.win[0] and not self.draw:
+            self.print_invitation_to_move()
+            self.input_user_move()
+            self.set_move()
+            self.display_playing_field()
 
+            if self.order == 1:
+                self.order = 2
+            elif self.order == 2:
+                self.order = 1
 
-	def print_hello(self):
-		""" Print hello-message to user(s). """
-		print("Hello! You are in tic tac toe. Press 'Enter' to start game!")
+            self.check_draw()
+            self.check_win()
 
-
-	def print_invitation_to_move(self):
-		""" Print invitation for users. """
-		if self.order == 1:
-			print("Move for first player.")
-		elif self.order == 2:
-			print("Move for second player.")
-		else:
-			return False
-
-		print("Choose free cell.")
-
-
-	def play(self):
-		""" The main part. Start game process. """
-		self.print_hello()
-		self.display_playing_field()
-
-		while self.win[0] != True and self.draw != True:
-			self.print_invitation_to_move()
-			self.input_user_move()
-			self.set_move()
-			self.display_playing_field()
-
-			if self.order == 1:
-				self.order = 2
-			elif self.order == 2:
-				self.order = 1
-
-			self.check_draw()
-			self.check_win()
-
-		self.print_congratulation()
-
+        self.print_congratulation()
 
 if __name__ == '__main__':
-	game = TicTacToe()
-	game.play()
+    game = TicTacToe()
+    game.play()
